6729d69bbe39e9f4a1ad43958036c74e43715a62
Chamoli <godchamoli@gmail.com>
2024-01-30 16:48:05 +0530

    Resolved the cors issues

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import numpy as np
 import cv2
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/', methods=['GET','POST'])
 def detect_face():
